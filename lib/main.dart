e6372ac97b2af012f51459856d20b8b6db3df88c
diilshadkt <dilshadktkunnathodi@gmail.com>
2024-12-31 15:39:54 +0530

    added firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:chateo/features/auth/view/pages/landing_page.dart';
 import 'package:chateo/features/nav/view/pages/navigation_page.dart';
+import 'package:chateo/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
