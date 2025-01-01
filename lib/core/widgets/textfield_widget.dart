2c9ac412e0687c51663754746bf2ae44eb25d2ef
diilshadkt <dilshadktkunnathodi@gmail.com>
2025-01-01 19:47:42 +0530

    text field addedin sign up page

--- lib/core/widgets/textfield_widget.dart
+++ lib/core/widgets/textfield_widget.dart
@@ -1,17 +1,28 @@
+import 'package:chateo/core/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class TextfieldWidget extends StatelessWidget {
   final String? label;
   final Widget? icon;
-  const TextfieldWidget({super.key, this.label, this.icon,});
+  final String? hintText;
+  const TextfieldWidget({
+    super.key,
+    this.label,
+    this.icon,
+    this.hintText,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return  TextField(
-      decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: label,
-          prefixIcon: icon),
+    final colors = AppTheme.of(context).colors;
+    final typography = AppTheme.of(context).typography;
+    return Container(
+      height: MediaQuery.sizeOf(context).height / 20,
+      color: colors.textInverse,
+      child: TextField(
+        decoration: InputDecoration(
+            border: InputBorder.none, hintText: label, prefixIcon: icon),
+      ),
     );
   }
 }
--- lib/features/auth/view/pages/signup_page.dart
+++ lib/features/auth/view/pages/signup_page.dart
@@ -1,5 +1,7 @@
 import 'package:chateo/core/constants/constants.dart';
 import 'package:chateo/core/themes/app_theme.dart';
+import 'package:chateo/core/widgets/textfield_widget.dart';
+import 'package:chateo/features/auth/view/widgets/textfield_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,20 +18,31 @@ class SignupPage extends ConsumerWidget {
     return Scaffold(
       backgroundColor: colors.secondary,
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.sizeOf(context).height / 7,
-            ),
-            Text(
-              constants.txtHeading,
-              style: typography.h800,
-            ),
-            Text(
-              constants.txtSubHeading,
-              style: typography.h100.copyWith(color: colors.text),
-            )
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.sizeOf(context).width / 11),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.sizeOf(context).height / 7,
+              ),
+              Text(
+                constants.txtHeading,
+                style: typography.h800,
+              ),
+              Text(
+                constants.txtSubHeading,
+                style: typography.h100.copyWith(color: colors.text),
+              ),
+              TextfieldWidget(label: constants.txtUserName),
+              const SizedBox(
+                height: 15,
+              ),
+              TextfieldWidget(label: constants.txtEmail),
+              TextfieldWidget(label: constants.txtPassword),
+              TextfieldWidget(label: constants.txtConfirmPassword),
+            ],
+          ),
         ),
       ),
     );
