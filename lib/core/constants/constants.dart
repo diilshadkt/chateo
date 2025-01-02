1a6c3a90c3146b6c1539439e670bfcfb92a2e16d
diilshadkt <dilshadktkunnathodi@gmail.com>
2025-01-02 13:15:56 +0530

    Login page ui completed

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,7 +2,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'constants.g.dart';
 
 class Constants {
-  // Login
+  // Signup
   final String txtHeading = "Register";
   final String txtSubHeading = "You and Your Friends always Connected";
   final String txtContinueBtn = "Continue";
@@ -12,6 +12,12 @@ class Constants {
   final String txtConfirmPassword = "Confirm Password";
   final String txtAlreadyHaveAccount = "Already have an account?";
   final String txtLogin = "Log In";
+  // Login
+   final String txtLoginHeading = "Login";
+  final String txtLoginSubHeading = "Remember to get up & stretch once in a while - Your friends at chat";
+  
+  final String txtDontHaveAccount = "Don't have an account?";
+  final String txtSignup = "Sign Up";
 }
 
 @riverpod
--- lib/features/auth/view/pages/login_page.dart
+++ lib/features/auth/view/pages/login_page.dart
@@ -1,10 +1,100 @@
+import 'package:chateo/core/constants/constants.dart';
+import 'package:chateo/core/themes/app_theme.dart';
+import 'package:chateo/core/widgets/button_widget.dart';
+import 'package:chateo/core/widgets/textfield_widget.dart';
+import 'package:chateo/features/auth/view/pages/login_page.dart';
+import 'package:chateo/features/auth/view/pages/signup_page.dart';
+import 'package:chateo/features/auth/view/widgets/textfield_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final constants = ref.watch(constantsProvider);
+    final colors = AppTheme.of(context).colors;
+    final typography = AppTheme.of(context).typography;
+    final spacer = AppTheme.of(context).spaces;
+
+    return Scaffold(
+      backgroundColor: colors.secondary,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.sizeOf(context).width / 11),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 5,
+                ),
+                Text(
+                  constants.txtLoginHeading,
+                  style: typography.h800,
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 80,
+                ),
+                Text(
+                  constants.txtLoginSubHeading,
+                  style: typography.h100.copyWith(color: colors.text),
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 13,
+                ),
+
+                TextfieldWidget(label: constants.txtEmail),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 40,
+                ),
+                TextfieldWidget(label: constants.txtPassword),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 40,
+                ),
+
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 13,
+                ),
+                ButtonWidget(onPressed: () {}),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 40,
+                ),
+                // Already have an account part
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      constants.txtDontHaveAccount,
+                      style: typography.h100.copyWith(color: colors.text),
+                    ),
+                    SizedBox(
+                      width: spacer.space_25,
+                    ),
+                    InkWell(
+                      child: Text(
+                        constants.txtSignup,
+                        style: typography.h200.copyWith(
+                            color: colors.primary,
+                            decoration: TextDecoration.underline),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignupPage(),
+                            ));
+                      },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
