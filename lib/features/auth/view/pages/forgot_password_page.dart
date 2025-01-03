ba249433926e292307514a5422b7645a5d5b1757
diilshadkt <dilshadktkunnathodi@gmail.com>
2025-01-03 15:42:23 +0530

    added firestore package

--- lib/features/auth/view/pages/forgot_password_page.dart
+++ lib/features/auth/view/pages/forgot_password_page.dart
@@ -5,10 +5,8 @@ import 'package:chateo/core/widgets/textfield_widget.dart';
 import 'package:chateo/features/auth/controller/auth_controller.dart';
 import 'package:chateo/features/auth/view/pages/login_page.dart';
 import 'package:chateo/features/auth/view/pages/signup_page.dart';
-import 'package:chateo/features/auth/view/widgets/forgot_password_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
